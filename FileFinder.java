6b6ba3000063e830903058356ec001cceafe16fd
James Wang <jameswang9909@hotmail.com>
2016-01-22 19:41:18 -0500

    Add method finder

--- FileFinder.java
+++ FileFinder.java
@@ -1,7 +1,30 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class FileFinder {
+
+    public static String readFile(String path) {
+        String contents = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            contents = sb.toString();
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            System.exit(1);
+        }
+        return contents;
+    }
 
     public static void findFiles(String path, ArrayList<String> fileNames, String ending) {
         File directory = new File(path);
--- UMLDiagram.java
+++ UMLDiagram.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UMLDiagram implements Diagram {
 
+    public String generateDiagram() {return "";}
+
+    private ArrayList<Method> methods;
+    private ArrayList<Variable> variables;
+
+    public void findMethods(String path) {
+        String contents = FileFinder.readFile(path);
+        Matcher matcher = Pattern.compile("((?:public|protected|private|abstract|\\s)(?:\\sstatic)?) +([\\w\\<\\>\\[\\]]+)\\s+(\\w+) *\\(([^\\)]*)\\) *(\\{?|[^;])").matcher(contents);
+        while (matcher.find()) {
+            String modifier = matcher.group(1).trim();
+            String returnType = matcher.group(2).trim();
+            String name = matcher.group(3).trim();
+            ArrayList<String> parameters = new ArrayList<String>(Arrays.asList(matcher.group(4).trim().split(",")));
+            Method method = new Method(name, returnType, modifier, parameters);
+            methods.add(method);
+        }
+    }
+
+    public static void main(String[] args) {
+        UMLDiagram diagram = new UMLDiagram();
+        diagram.findMethods("UMLDiagram.java");
+    }
 }
