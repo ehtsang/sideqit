8c42d2068db848ff961a055959ee40ee3d736070
James Wang <jameswang9909@hotmail.com>
2016-01-24 20:00:45 -0500

    Fix regex for constructors

--- UMLDiagram.java
+++ UMLDiagram.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -9,17 +10,36 @@ public class UMLDiagram implements Diagram {
     public String generateDiagram() {return "";}
 
     public ArrayList<Method> findMethods(String path) {
+        String name, modifier, returnType;
+
         ArrayList<Method> methods = new ArrayList<Method>();
         String contents = FileFinder.readFile(path);
-        Matcher matcher = Pattern.compile("((?:public|protected|private|abstract|\\s)(?:\\sstatic)?) +([\\w\\<\\>\\[\\]]+)\\s+(\\w+) *\\(([^\\)]*)\\) *(\\{?|[^;])").matcher(contents);
+
+        String className = new File(path).getName();
+        className = className.substring(0, className.length()-5);
+
+        // Regex for constructors
+        Matcher matcher = Pattern.compile("(?:public) " + className + "\\(([^\\)]*)\\) *(\\{?|[^;])").matcher(contents);
         while (matcher.find()) {
-            String modifier = matcher.group(1).trim();
-            String returnType = matcher.group(2).trim();
-            String name = matcher.group(3).trim();
+            modifier = "public";
+            returnType = "";
+            name = className;
+            ArrayList<String> parameters = new ArrayList<String>(Arrays.asList(matcher.group(1).trim().split(",")));
+            Method constructor = new Method(name, "", modifier, parameters);
+            methods.add(constructor);
+        }
+
+        // Regex for other methods
+        matcher = Pattern.compile("((?:public|protected|private|static|final)+) +([\\w\\<\\>\\[\\]]+) +(\\w+) *\\(([^\\)]*)\\) *(\\{?|[^;])").matcher(contents);
+        while (matcher.find()) {
+            modifier = matcher.group(1).trim();
+            returnType = matcher.group(2).trim();
+            name = matcher.group(3).trim();
             ArrayList<String> parameters = new ArrayList<String>(Arrays.asList(matcher.group(4).trim().split(",")));
             Method method = new Method(name, returnType, modifier, parameters);
             methods.add(method);
         }
+
         return methods;
     }
 
@@ -32,7 +52,7 @@ public class UMLDiagram implements Diagram {
             String type = matcher.group(2).trim();
             String name = matcher.group(3).trim();
             // System.out.println(modifier); //TESTING PURPOSES
-	    Variable variable = new Variable(name, modifier, type);
+            Variable variable = new Variable(name, modifier, type);
             variables.add(variable);
         }
         return variables;
@@ -40,17 +60,17 @@ public class UMLDiagram implements Diagram {
 
     public static void main(String[] args) {
         ArrayList<Method> methods = new ArrayList<Method>();
-	ArrayList<Variable> variables = new ArrayList<Variable>();
-	UMLDiagram diagram = new UMLDiagram();
-	methods = diagram.findMethods("Method.java");
-	variables = diagram.findVariables("Method.java");
-    	System.out.println("-->Methods");
-	for(Method method : methods){
-		System.out.println("--|" + method);
-	}
-	System.out.println("-->Variables");
-	for(Variable variable : variables){
-		System.out.println("--|" + variable);
-	}
+        ArrayList<Variable> variables = new ArrayList<Variable>();
+        UMLDiagram diagram = new UMLDiagram();
+        methods = diagram.findMethods("Method.java");
+        variables = diagram.findVariables("Method.java");
+        System.out.println("-->Methods");
+        for(Method method : methods){
+            System.out.println("--|" + method);
+        }
+        System.out.println("-->Variables");
+        for(Variable variable : variables){
+            System.out.println("--|" + variable);
+        }
     }
 }
